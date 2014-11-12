4102eef176a87a8c9c6b5222b611b74aed8ecf17
Marco Lucio <mlucio@gmail.com>
2014-11-11 21:38:02 -0600

    Avance, ya casi terminado

--- RenombraFacturas/frmMain.Designer.cs
+++ RenombraFacturas/frmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RenombraFacturas
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,61 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.renombrar = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnRenombrarArchivos = new System.Windows.Forms.Button();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
             this.RFCEmisor = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.NombreEmisor = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.NombreArchivo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.NuevoNombreArchivo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.UUID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.DirectorioFacturas = new System.Windows.Forms.TextBox();
+            this.btnSeleccionarDirectorio = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
             this.SuspendLayout();
             // 
-            // renombrar
+            // btnRenombrarArchivos
             // 
-            this.renombrar.Location = new System.Drawing.Point(755, 27);
-            this.renombrar.Name = "renombrar";
-            this.renombrar.Size = new System.Drawing.Size(133, 23);
-            this.renombrar.TabIndex = 7;
-            this.renombrar.Text = "Renombrar archivos";
-            this.renombrar.UseVisualStyleBackColor = true;
-            this.renombrar.Click += new System.EventHandler(this.renombrar_Click);
+            this.btnRenombrarArchivos.Location = new System.Drawing.Point(755, 27);
+            this.btnRenombrarArchivos.Name = "btnRenombrarArchivos";
+            this.btnRenombrarArchivos.Size = new System.Drawing.Size(133, 23);
+            this.btnRenombrarArchivos.TabIndex = 7;
+            this.btnRenombrarArchivos.Text = "Renombrar archivos";
+            this.btnRenombrarArchivos.UseVisualStyleBackColor = true;
+            this.btnRenombrarArchivos.Click += new System.EventHandler(this.renombrar_Click);
             // 
-            // dataGridView1
+            // dgvDatos
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.RFCEmisor,
             this.NombreEmisor,
             this.NombreArchivo,
             this.NuevoNombreArchivo,
             this.Fecha,
             this.UUID});
-            this.dataGridView1.Location = new System.Drawing.Point(31, 72);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(961, 282);
-            this.dataGridView1.TabIndex = 6;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(50, 30);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(549, 20);
-            this.textBox1.TabIndex = 5;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(605, 27);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Seleccionar Directorio";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.dgvDatos.Location = new System.Drawing.Point(31, 72);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.Size = new System.Drawing.Size(961, 282);
+            this.dgvDatos.TabIndex = 6;
             // 
             // RFCEmisor
             // 
@@ -119,18 +101,36 @@
             this.UUID.Name = "UUID";
             this.UUID.Visible = false;
             // 
-            // Form1
+            // DirectorioFacturas
+            // 
+            this.DirectorioFacturas.Location = new System.Drawing.Point(50, 30);
+            this.DirectorioFacturas.Name = "DirectorioFacturas";
+            this.DirectorioFacturas.ReadOnly = true;
+            this.DirectorioFacturas.Size = new System.Drawing.Size(549, 20);
+            this.DirectorioFacturas.TabIndex = 5;
+            // 
+            // btnSeleccionarDirectorio
+            // 
+            this.btnSeleccionarDirectorio.Location = new System.Drawing.Point(605, 27);
+            this.btnSeleccionarDirectorio.Name = "btnSeleccionarDirectorio";
+            this.btnSeleccionarDirectorio.Size = new System.Drawing.Size(133, 23);
+            this.btnSeleccionarDirectorio.TabIndex = 4;
+            this.btnSeleccionarDirectorio.Text = "Seleccionar Directorio";
+            this.btnSeleccionarDirectorio.UseVisualStyleBackColor = true;
+            this.btnSeleccionarDirectorio.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1016, 382);
-            this.Controls.Add(this.renombrar);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Controls.Add(this.btnRenombrarArchivos);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.DirectorioFacturas);
+            this.Controls.Add(this.btnSeleccionarDirectorio);
+            this.Name = "frmMain";
             this.Text = "Renombra Facturas";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,10 +138,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button renombrar;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRenombrarArchivos;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.TextBox DirectorioFacturas;
+        private System.Windows.Forms.Button btnSeleccionarDirectorio;
         private System.Windows.Forms.DataGridViewTextBoxColumn RFCEmisor;
         private System.Windows.Forms.DataGridViewTextBoxColumn NombreEmisor;
         private System.Windows.Forms.DataGridViewTextBoxColumn NombreArchivo;
---
+++ RenombraFacturas/frmMain.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Collections;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.Text.RegularExpressions;
+
+namespace RenombraFacturas
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+            int j = 0;
+
+            DirectorioFacturas.Text = fbd.SelectedPath;
+            dgvDatos.Rows.Clear();
+            string[] files = Directory.GetFiles(fbd.SelectedPath,"*.xml");
+            // Load the file names to the box
+            foreach (string file in files)
+            {
+                dgvDatos.Rows.Add();
+                ReadFile(file, j);
+                j++;
+            }
+        }
+
+        private void ReadFile(string file, int j)
+        {
+            string rfc="", serie="", folio="", fecha="";
+            XmlTextReader XmlTextReader = new XmlTextReader(file);
+            dgvDatos.Rows[j].Cells[2].Value = Path.GetFileNameWithoutExtension(file);
+            while (XmlTextReader.Read())
+            {
+                if (XmlTextReader.IsStartElement())
+                {
+                    switch (XmlTextReader.Name)
+                    {
+                        case "cfdi:Emisor":
+                            dgvDatos.Rows[j].Cells[0].Value = (XmlTextReader.GetAttribute("rfc"));
+                            dgvDatos.Rows[j].Cells[1].Value = (XmlTextReader.GetAttribute("nombre"));
+                            rfc = (XmlTextReader.GetAttribute("rfc"));
+                            break;
+                        case "cfdi:Comprobante":
+                            serie = (XmlTextReader.GetAttribute("serie"));
+                            folio = (XmlTextReader.GetAttribute("folio"));
+                            break;
+                        case "cfdi:DomicilioFiscal":
+                            break;
+                        case "tfd:TimbreFiscalDigital":
+                            dgvDatos.Rows[j].Cells[5].Value = (XmlTextReader.GetAttribute("UUID"));
+                            fecha = (XmlTextReader.GetAttribute("FechaTimbrado"));
+                            break;
+                    }
+                }
+
+            }
+            dgvDatos.Rows[j].Cells[3].Value = rfc + "_" + serie + folio + "_" + DateTime.ParseExact(fecha, "yyyy-MM-ddTHH:mm:ss",
+                                           System.Globalization.CultureInfo.InvariantCulture).ToString("yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture);
+            dgvDatos.Rows[j].Cells[4].Value = fecha;
+            XmlTextReader.Close();
+        }
+
+        private void renombrar_Click(object sender, EventArgs e)
+        {
+            int j = 0;
+            string[] PDFfiles = Directory.GetFiles(DirectorioFacturas.Text, "*.pdf");
+            List<string> list = new List<string>(PDFfiles);
+
+            for (j = 0; j < dgvDatos.Rows.Count - 1; j++)
+            {
+                if (File.Exists(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[2].Value.ToString() + ".xml") &&
+                     !File.Exists(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value + ".xml"))
+                {
+                    System.IO.File.Move(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[2].Value.ToString() + ".xml", DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value.ToString() + ".xml");
+                    File.SetCreationTime(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value + ".xml", DateTime.ParseExact(dgvDatos.Rows[j].Cells[4].Value.ToString(), "yyyy-MM-ddTHH:mm:ss",
+                                           System.Globalization.CultureInfo.InvariantCulture));
+                    File.SetLastWriteTime(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value + ".xml", DateTime.ParseExact(dgvDatos.Rows[j].Cells[4].Value.ToString(), "yyyy-MM-ddTHH:mm:ss",
+                                           System.Globalization.CultureInfo.InvariantCulture));
+                }
+                if (File.Exists(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[2].Value.ToString() + ".pdf") &&
+                    !File.Exists(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value + ".pdf"))
+                {
+                    System.IO.File.Move(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[2].Value.ToString() + ".pdf", DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value.ToString() + ".pdf");
+                    File.SetCreationTime(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value + ".pdf", DateTime.ParseExact(dgvDatos.Rows[j].Cells[4].Value.ToString(), "yyyy-MM-ddTHH:mm:ss",
+                                           System.Globalization.CultureInfo.InvariantCulture));
+                    File.SetLastWriteTime(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value + ".pdf", DateTime.ParseExact(dgvDatos.Rows[j].Cells[4].Value.ToString(), "yyyy-MM-ddTHH:mm:ss",
+                                           System.Globalization.CultureInfo.InvariantCulture));
+                    list.Remove(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[2].Value.ToString() + ".pdf");
+                    dgvDatos.Rows[j].Cells[5].Value = "";
+                } 
+            }
+            for (j = 0; j < dgvDatos.Rows.Count - 1; j++)
+            {
+                if (!dgvDatos.Rows[j].Cells[5].Value.Equals(""))
+                foreach (string file in list)
+                {
+                    PdfReader reader = new PdfReader(file);
+                    int numeroDePaginas;
+                    string text = "";
+                    numeroDePaginas = reader.NumberOfPages;
+                    for (int i = 0; i < numeroDePaginas; i++)
+                    {
+                        text += PdfTextExtractor.GetTextFromPage(reader, i + 1);
+                    }
+                    try { reader.Close(); }
+                    catch { }
+                    Regex regex = new Regex(dgvDatos.Rows[j].Cells[5].Value.ToString());
+                    MatchCollection matches = regex.Matches(text);
+                    if(matches.Count > 0)
+                    {
+                        if (!File.Exists(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value + ".pdf"))
+                        {
+                            System.IO.File.Move(file, DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value.ToString() + ".pdf");
+                            File.SetCreationTime(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value + ".pdf", DateTime.ParseExact(dgvDatos.Rows[j].Cells[4].Value.ToString(), "yyyy-MM-ddTHH:mm:ss",
+                                                   System.Globalization.CultureInfo.InvariantCulture));
+                            File.SetLastWriteTime(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value + ".pdf", DateTime.ParseExact(dgvDatos.Rows[j].Cells[4].Value.ToString(), "yyyy-MM-ddTHH:mm:ss",
+                                                   System.Globalization.CultureInfo.InvariantCulture));
+                            list.Remove(file);
+                            break;
+                        }
+                        else
+                        {
+                            int i = 1;
+                            while (File.Exists(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value + "_" + i.ToString() + ".pdf"))
+                            {
+                                i++;
+                            }
+                            System.IO.File.Move(file, DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value.ToString() + "_" + i.ToString() + ".pdf");
+                            File.SetCreationTime(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value + "_" + i.ToString() + ".pdf", DateTime.ParseExact(dgvDatos.Rows[j].Cells[4].Value.ToString(), "yyyy-MM-ddTHH:mm:ss",
+                                                   System.Globalization.CultureInfo.InvariantCulture));
+                            File.SetLastWriteTime(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value + "_" + i.ToString() + ".pdf", DateTime.ParseExact(dgvDatos.Rows[j].Cells[4].Value.ToString(), "yyyy-MM-ddTHH:mm:ss",
+                                                   System.Globalization.CultureInfo.InvariantCulture));
+                            list.Remove(file);
+                            break;
+                        }
+                    }
+                }
+            }
+            foreach (string file in list)
+            {
+                PdfReader reader = new PdfReader(file);
+                int numeroDePaginas;
+                string text = "";
+                numeroDePaginas = reader.NumberOfPages;
+                for (int i = 0; i < numeroDePaginas; i++)
+                {
+                    text += PdfTextExtractor.GetTextFromPage(reader, i + 1);
+                }
+                try { reader.Close(); }
+                catch { }
+                Regex regex = new Regex(@"\s*(?<FolioFiscal>[A-Z0-9]+\-[A-Z0-9]+\-[A-Z0-9]+\-[A-Z0-9]+\-[A-Z0-9]+)\s*");
+                MatchCollection matches = regex.Matches(text);
+                if(matches.Count > 0)
+                for (j = 0; j < dgvDatos.Rows.Count - 1; j++)
+                {
+                    if (dgvDatos.Rows[j].Cells[5].Value.Equals(matches[0].Groups["FolioFiscal"].Value))
+                    {
+                        int i = 1;
+                        while (File.Exists(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value + "_" + i.ToString() + ".pdf"))
+                        {
+                            i++;
+                        }
+                        System.IO.File.Move(file, DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value.ToString() + "_" + i.ToString() + ".pdf");
+                        File.SetCreationTime(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value + "_" + i.ToString() + ".pdf", DateTime.ParseExact(dgvDatos.Rows[j].Cells[4].Value.ToString(), "yyyy-MM-ddTHH:mm:ss",
+                                               System.Globalization.CultureInfo.InvariantCulture));
+                        File.SetLastWriteTime(DirectorioFacturas.Text + "\\" + dgvDatos.Rows[j].Cells[3].Value + "_" + i.ToString() + ".pdf", DateTime.ParseExact(dgvDatos.Rows[j].Cells[4].Value.ToString(), "yyyy-MM-ddTHH:mm:ss",
+                                               System.Globalization.CultureInfo.InvariantCulture));
+                        //list.Remove(file);
+                        break;
+                    }
+                }
+            }
+            MessageBox.Show("Archivos Renombrados.");
+        }
+
+    }
+}
